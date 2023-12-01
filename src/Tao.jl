dfc370eacc9ee398e037da8dd6c23d6332ee3161
Matt Signorelli <mgs255@cornell.edu>
2023-11-30 19:51:10 -0500

    BAGELS_2 added

--- src/Tao.jl
+++ src/Tao.jl
@@ -6,24 +6,24 @@ using Printf
 export BAGELS_1, metadata_path
 
 function metadata_path(lat)
-  path = homedir() * "/.tao.jl" * pwd()[length(homedir())+1:end] * "/" * lat
+  path = homedir() * "/.tao_jl" * pwd()[length(homedir())+1:end] * "/" * lat
   if !ispath(path)
     mkpath(path)
   end
   return path
 end
 
-# Polarization routines ======================================
+# --- Polarization routines ---
 
 """
-    BAGELS_1(lat, phi_diff, phi_start, sgn)
+    BAGELS_1(lat, phi_start, phi_step, sgn, kick=1e-5, tol=1e-8)
 
-Calculates the response of dn/ddelta with all combinations of closed orbit 2-bumps.
-Coil pairs with phase separation of dPhi are included when 
-`rem(dPhi - phi_start, phi_step, RoundNearest) < tol`. `sgn` specifies the sign of the first
-kick times the sign of the second kick. E.g., for only pi-bumps, use `phi_start` = pi, 
-`phi_step` = (something large), and `sgn` = -1. For all orthogonal kicks (equal kicks 
-2*pi*N apart), use `phi_start` = 0, `phi_step` = 2*pi, `sgn` = 1.
+Best Adjustment Groups for ELectron Spin (BAGELS) method step 1: calculates the response of 
+dn/ddelta with all combinations of closed orbit 2-bumps. Coil pairs with phase separation of 
+dPhi are included when `rem(dPhi - phi_start, phi_step, RoundNearest) < tol`. `sgn` specifies 
+the sign of the first kick times the sign of the second kick. E.g., for only pi-bumps, use 
+`phi_start` = pi, `phi_step` = (something large), and `sgn` = -1. For all orthogonal kicks 
+(equal kicks 2*pi*N apart), use `phi_start` = 0, `phi_step` = 2*pi, `sgn` = 1.
 
 ### Input
 - `lat`       -- lat file name
@@ -46,60 +46,140 @@ function BAGELS_1(lat, phi_start, phi_step, sgn, kick=1e-5, tol=1e-8)
       run(`tao -lat $lat -noplot -command "show -write $(path)/kicks.txt lat vkicker::* -at phi_b@f20.16 -at kick@f20.16; exit"`)
     end
 
-  # Read coil data and extract valid pairs
-  coil_data = readdlm("$(path)/vkickers.txt", skipstart=2)
-  coil_names = coil_data[1:end-2, 2]
-  coil_phis = coil_data[1:end-2, 6]
-  coil_kicks = readdlm("$(path)/kicks.txt", skipstart=2)[1:end-2,7]
+    # Read coil data and extract valid pairs
+    coil_data = readdlm("$(path)/vkickers.txt", skipstart=2)
+    coil_names = coil_data[1:end-2, 2]
+    coil_phis = coil_data[1:end-2, 6]
+    coil_kicks = readdlm("$(path)/kicks.txt", skipstart=2)[1:end-2,7]
 
-  coil_pairs_list = []
-  coil_kicks_list = []
+    coil_pairs_list = []
+    coil_kicks_list = []
 
-  for i=1:length(coil_names)-1
-    for j=i+1:length(coil_names)
-      if abs(rem(coil_phis[j] - coil_phis[i] - phi_start, phi_step, RoundNearest)) < 1e-8
-        push!(coil_pairs_list, coil_names[i])
-        push!(coil_pairs_list, coil_names[j])
-        push!(coil_kicks_list, coil_kicks[i])
-        push!(coil_kicks_list, coil_kicks[j])
+    for i=1:length(coil_names)-1
+      for j=i+1:length(coil_names)
+        if abs(rem(coil_phis[j] - coil_phis[i] - phi_start, phi_step, RoundNearest)) < tol
+          push!(coil_pairs_list, coil_names[i])
+          push!(coil_pairs_list, coil_names[j])
+          push!(coil_kicks_list, coil_kicks[i])
+          push!(coil_kicks_list, coil_kicks[j])
+        end
       end
     end
+
+    coil_pairs = permutedims(reshape(coil_pairs_list, 2, Int(length(coil_pairs_list)/2)))
+    strengths = permutedims(reshape(coil_kicks_list, 2, Int(length(coil_pairs_list)/2)))
+
+    writedlm("$(path)/bumps$(str_phi).txt", hcat(coil_pairs, strengths), ',')
   end
 
-  coil_pairs = permutedims(reshape(coil_pairs_list, 2, Int(length(coil_pairs_list)/2)))
-  strengths = permutedims(reshape(coil_kicks_list, 2, Int(length(coil_pairs_list)/2)))
+  coils = readdlm("$(path)/bumps$(str_phi).txt", ',')
+  coil_pairs = coils[:,1:2]
+  strengths = coils[:,3:4]
+  if isdir("$(path)/responses_$(str_kick)")
+    mkdir("$(path)/responses_$(str_kick)")
+  end
+  # All of these bumps are separate group knobs, the individual coils have opposite strengths
+  # Now build response matrix of dn/ddelta at each sbend (sampled at beginning and ends of bends)
+  tao_cmd = open("$(path)/BAGELS_1.cmd", "w")
+  println(tao_cmd, "show -write $(path)/responses_$(str_kick)/baseline.txt lat sbend::* multipole::* -at spin_dn_dpz.x@f20.16 -at spin_dn_dpz.y@f20.16 -at spin_dn_dpz.z@f20.16")
 
-  writedlm("$(path)/bumps$(str_phi).txt", hcat(coil_pairs, strengths), ',')
+  for i=1:length(coil_pairs[:,1])
+    coil1 = coil_pairs[i,1]
+    coil2 = coil_pairs[i,2]
+    strength1 = strengths[i,1]
+    strength2 = strengths[i,2]
+    println(tao_cmd, "set ele $(coil1) kick = $(strength1 + kick)")
+    println(tao_cmd, "set ele $(coil2) kick = $(strength2 + sgn*kick)")
+    println(tao_cmd, "show -write $(path)/responses_$(str_kick)/$(coil1)_$(coil2).txt lat sbend::* multipole::* -at spin_dn_dpz.x@f20.16 -at spin_dn_dpz.y@f20.16 -at spin_dn_dpz.z@f20.16")
+    println(tao_cmd, "set ele $(coil1) kick = $(strength1)")
+    println(tao_cmd, "set ele $(coil2) kick = $(strength2)")
+  end
+  println(tao_cmd, "exit")
+  close(tao_cmd)
+
+  run(`tao -lat $lat -noplot -command "call $(path)/BAGELS_1.cmd"`)
 end
 
-coils = readdlm("$(path)/bumps$(str_phi).txt", ',')
-coil_pairs = coils[:,1:2]
-strengths = coils[:,3:4]
-if isdir("$(path)/responses_$(str_kick)")
-  mkdir("$(path)/responses_$(str_kick)")
+"""
+    BAGELS_2(lat, phi_start, phi_step, kick=1e-5)
+
+Best Adjustment Groups for ELectron Spin (BAGELS) method step 2: peform an SVD of the 
+response matrix to obtain the best adjustment groups, based on the settings of step 1. 
+
+
+### Input
+- `lat`       -- lat file name
+- `phi_start` -- phi_start as described above
+- `phi_step`  -- phi_step as described above
+- `N_knobs`   -- Number of knobs to generate in group element, written in Bmad format
+- `suffix`    -- (Optional) Suffix to append to group elements generated for knobs in Bmad format
+- `outf`      -- (Optional) Output file name with group elements constructed from BAGELS, default is "BAGELS.bmad"
+- `kick`      -- (Optional) Coil kick, default is 1e-5
+"""
+function BAGELS_2(lat, phi_start, phi_step, suffix="",outf="BAGELS.bmad", kick=1e-5)
+  path = metadata_path(lat)
+  str_phi = @sprintf("%1.2e", phi_start) * "_" * @sprintf("%1.2e", phi_step)
+  str_kick = @sprintf("%1.2e", kick)
+
+  coil_pairs = readdlm("$(path)/bumps$(str_phi).txt", ',')[:,1:2]
+  eletypes = readdlm("$(path)/responses_$(str_kick)/baseline.txt", skipstart=2)[1:end-2,3]
+  dn_dpz0 = readdlm("$(path)/responses_$(str_kick)/baseline.txt", skipstart=2)[1:end-2,6:8]
+
+  # Get number of Sbends to only sample at Sbends
+  idx_bends = findall(i-> i=="Sbend", eletypes)
+  dn_dpz0_bends = dn_dpz0[idx_bends,:]
+
+  # We now will have a response matrix that is N_bends x N_coil_pairs to multiply with vector N_coil_pairs x 1
+  delta_dn_dpz = zeros(length(idx_bends), length(coil_pairs[:,1]))
+  for i=1:length(coil_pairs[:,1])
+    coil1 = coil_pairs[i,1]
+    coil2 = coil_pairs[i,2]
+    dn_dpz_bends = readdlm("$(path)/responses_$(str_kick)/$(coil1)_$(coil2).txt", skipstart=2)[1:end-2,6:8][idx_bends,:]
+    #dn_dpz_bends_amp = [norm(dn_dpz_bends[j,:]) for j=1:length(dn_dpz_bends[:,1])]
+    #delta_dn_dpz_amp = dn_dpz_bends_amp .- dn_dpz0_bends_amp
+    #delta_dn_dpz_amp = [norm(delta_dn_dpz[j,:]) for j=1:length(delta_dn_dpz[:,1])]
+    # For now, BAGELS only uses x component of dn/ddelta
+    delta_dn_dpz[:,i] = (dn_dpz_bends[:,1] .- dn_dpz0_bends[:,1])./kick
+  end
+  
+  # Now do SVD to get the principal components
+  F = svd(delta_dn_dpz)
+
+  # Get first N_knobs principal directions
+  V = F.V[:,1:N_knobs]
+  # Create a group element with these as knobs
+  for i=1:N_knobs
+    knob = V[:,i]
+    raw = Matrix{Any}(undef, length(coil_pairs), 2) #zeros(length(coil_pairs), 2)
+    # First column of raw contains coil name, second is knob value
+    for j=1:length(coil_pairs[:,1])
+      coil1 = coil_pairs[j,1]
+      coil2 = coil_pairs[j,2]
+      raw[2*j-1, 1] = coil1
+      raw[2*j-1, 2] = knob[j]
+      raw[2*j, 1] = coil2
+      raw[2*j, 2] = -knob[j]
+    end
+
+    # Now make knobs for each
+    unique_coils = unique(coil_pairs)
+
+    if i == 1
+      knob_out = open(outf, "w")
+    else
+      knob_out = open(outf, "a")
+    end
+    println(knob_out, "BAGELS$(suffix)$(@sprintf("%0$(length(string(i)))", i)): group = {")
+
+    for coil in unique_coils
+      strength = 0.
+      idxs_coil = findall(i->i==coil, raw[:,1])
+      strength = sum(raw[idxs_coil,2])
+      println(knob_out, "$(coil)[kick]: $(strength)*X,")
+    end
+    println(knob_out, "}, var = {X}")
+    close(knob_out)
+  end
 end
-# All of these bumps are separate group knobs, the individual coils have opposite strengths
-# Now build response matrix of dn/ddelta at each sbend (sampled at beginning and ends of bends)
-tao_cmd = open("$(path)/BAGELS_1.cmd", "w")
-println(tao_cmd, "show -write $(path)/responses_$(str_kick)/baseline.txt lat sbend::* multipole::* -at spin_dn_dpz.x@f20.16 -at spin_dn_dpz.y@f20.16 -at spin_dn_dpz.z@f20.16")
-
-for i=1:length(coil_pairs[:,1])
-  coil1 = coil_pairs[i,1]
-  coil2 = coil_pairs[i,2]
-  strength1 = strengths[i,1]
-  strength2 = strengths[i,2]
-  println(tao_cmd, "set ele $(coil1) kick = $(strength1 + kick)")
-  println(tao_cmd, "set ele $(coil2) kick = $(strength2 - kick)")
-  println(tao_cmd, "show -write $(path)/responses_$(str_kick)/$(coil1)_$(coil2).txt lat sbend::* multipole::* -at spin_dn_dpz.x@f20.16 -at spin_dn_dpz.y@f20.16 -at spin_dn_dpz.z@f20.16")
-  println(tao_cmd, "set ele $(coil1) kick = $(strength1)")
-  println(tao_cmd, "set ele $(coil2) kick = $(strength2)")
-end
-println(tao_cmd, "exit")
-close(tao_cmd)
-
-run(`tao -lat $lat -noplot -command "call $(path)/BAGELS_1.cmd"`)
-end
-
-
 
 end
